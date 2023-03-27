d5285128ea35421a00d849a09c73df6148af097b
Ali Can Kayaaslan <ackayaaslan@gmail.com>
2023-03-27 18:06:43 +0300

    Initial Commit

---
+++ WhoAmI/WhoAmIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoAmIApp.swift
+//  WhoAmI
+//
+//  Created by Ali Can Kayaaslan on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhoAmIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
